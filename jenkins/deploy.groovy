f518c133efeda589ea9945a609cd214f8b451f39
satishthamarana <satish.thamarana@gmail.com>
2022-08-10 01:32:35 +0530

    Deploy files

--- jenkins/deploy.groovy
+++ jenkins/deploy.groovy
@@ -5,7 +5,7 @@ pipeline {
             steps {           
                         sh 'pwd'
                         sh 'cp -R helm/* .'
-		        sh 'ls -ltr'
+		                sh 'ls -ltra'
                         sh 'pwd'
                         sh '/usr/local/bin/helm install customer-service customer --set image.repository=registry.hub.docker.com/thamarana/customer-service --set image.tag=28'
 
